37e7f60d7c59effda554d26bc0d3746f27bf0f5e
Alexandre-CES <147560376+Alexandre-CES@users.noreply.github.com>
2024-11-01 21:43:33 -0300

    file organizer

--- src/modules/files/organizer/organizer.controller.ts
+++ src/modules/files/organizer/organizer.controller.ts
@@ -1,14 +1,80 @@
 import { Request, Response } from 'express';
 import fs from 'fs';
+import path from 'path';
+import { FileTypes } from './interfaces/file-types.interface';
 
 export default class OrganizerController {
-  public organize(req: Request, res: Response){
-    const filePath = req.body.filePath;
+  public async organize(req: Request, res: Response) {
+    const reqFilePath = req.body.filePath;
 
-    if (!fs.existsSync(filePath)){ 
-      res.status(400).send('Arquivo não encontrado.'); 
-    } 
-    
-    res.send(filePath);
+    if (!fs.existsSync(reqFilePath)) { 
+      return res.status(404).send('Path not found'); 
+    }
+
+    try {
+      const data = await fs.promises.readdir(reqFilePath);
+
+      // read file-types.json
+      const jsonFilePath = path.join(process.cwd(), 'src/modules/files/organizer/data/file-types.json');
+      const jsonData = await fs.promises.readFile(jsonFilePath);
+      const fileTypes: FileTypes = JSON.parse(jsonData.toString());
+
+      for (const file of data) {
+        const filePath = path.join(reqFilePath, file);
+        const stats = await fs.promises.stat(filePath);
+
+        // if isn't a directory
+        if (stats.isFile()) {
+          const ext = path.extname(file).toLowerCase();
+          let moved = false;
+
+          for (const directory in fileTypes) {
+            //stop loop if there are repeated extensions in different directories in the json file
+            if (moved) break; 
+
+            const fileExtensions: string[] = fileTypes[directory];
+
+            if (fileExtensions.includes(ext)) {
+              const dirPath = path.join(reqFilePath, directory);
+
+              //create dirpath if doesn't exist 
+              if (!fs.existsSync(dirPath)) {
+                fs.mkdirSync(dirPath, { recursive: true });
+              }
+
+              //copy file
+              const destFilePath = path.join(dirPath, file);
+              await fs.promises.copyFile(filePath, destFilePath);
+
+              //verify if copy exist before delete the original one
+              const copiedStats = await fs.promises.stat(destFilePath);
+              if (copiedStats.size === stats.size) {
+                await fs.promises.unlink(filePath);
+                moved = true;
+              }
+            }
+          }
+        }
+      }
+
+      res.send('Files organized successfully.');
+    } catch(err) {
+      if (err instanceof Error && 'code' in err) { 
+        const errorCode = (err as any).code;
+        switch (errorCode) { 
+          case 'ENOENT': 
+            res.status(404).send('File or directory not found.'); 
+            break; 
+          case 'EACCES': 
+            res.status(403).send('Permission denied.'); 
+            break; 
+          default: 
+            res.status(500).send('Internal server error.'); 
+            break; 
+        } 
+      } else { 
+        res.status(500).send('Unknown error.'); 
+      }
+    }
   }
 }
--- src/modules/files/organizer/organizer.module.ts
+++ src/modules/files/organizer/organizer.module.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import OrganizerController from './organizer.controller';
+import { asyncHandler } from '../../../middlewares/asyncHandler';
+import { Request, Response } from 'express-serve-static-core';
 
 const organizerController = new OrganizerController();
 const router = Router();
 
-router.get('/', (req, res) => organizerController.organize(req, res));
+router.post('/', asyncHandler((req: Request, res: Response) => organizerController.organize(req, res)));
 
 export default router;
