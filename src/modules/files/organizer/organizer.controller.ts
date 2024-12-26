7dd1d0a4612ee255fe568af28cd47db13e5fa809
Alexandre-CES <147560376+Alexandre-CES@users.noreply.github.com>
2024-12-26 12:56:36 -0300

    feat: customJson parameter at oeganizer, where user can custom how the files and folders should be organized

--- src/modules/files/organizer/organizer.controller.ts
+++ src/modules/files/organizer/organizer.controller.ts
@@ -9,7 +9,10 @@ export class OrganizerController {
   ){}
 
   public async organize(req: Request, res: Response) {
-    const result = await this.organizerService.organize(this.pathService.getDirPath());
+    const result = await this.organizerService.organize(
+      this.pathService.getDirPath(),
+      req.body.customJson
+    );
 
     res.status(result.status).send(result.body);
   }
--- src/modules/files/organizer/organizer.service.ts
+++ src/modules/files/organizer/organizer.service.ts
@@ -6,21 +6,27 @@ import { handleFileErrors } from '../helpers/handle-file-errors';
 
 export class OrganizerService{
 
-    async organize(baseDirPath: string): Promise<SimpleReturnObject>{
+    async organize(baseDirPath: string, customJson?:FileTypes): Promise<SimpleReturnObject>{
 
         try {
-            
+            //read directory
             const data = await fs.promises.readdir(baseDirPath);
 
-            // read file-types.json
-            const jsonFilePath = path.join(process.cwd(), 'src/modules/files/organizer/data/file-types.json');
-            const jsonData = await fs.promises.readFile(jsonFilePath);
-            const fileTypes: FileTypes = JSON.parse(jsonData.toString());
-    
+            let fileTypes: FileTypes;
+
+            if(customJson){
+                fileTypes = customJson;
+            }else{
+                 // read file-types.json
+                const jsonFilePath = path.join(process.cwd(), 'src/modules/files/organizer/json/file-types.json');
+                const jsonData = await fs.promises.readFile(jsonFilePath);
+                fileTypes = JSON.parse(jsonData.toString());
+            }
+
             for (const file of data) {
                 const filePath = path.join(baseDirPath, file);
                 const stats = await fs.promises.stat(filePath);
-    
+
                 // if isn't a directory
                 if (stats.isFile()) {
                     const ext = path.extname(file).toLowerCase();
@@ -29,21 +35,21 @@ export class OrganizerService{
                     for (const directory in fileTypes) {
                         //stop loop if there are repeated extensions in different directories in the json file
                         if (moved) break; 
-        
+
                         const fileExtensions: string[] = fileTypes[directory];
-        
+
                         if (fileExtensions.includes(ext)) {
                             const dirPath = path.join(baseDirPath, directory);
-            
+
                             //create dirpath if doesn't exist 
                             if (!fs.existsSync(dirPath)) {
                                 fs.mkdirSync(dirPath, { recursive: true });
                             }
-            
+
                             //copy file
                             const destFilePath = path.join(dirPath, file);
                             await fs.promises.copyFile(filePath, destFilePath);
-            
+
                             //verify if copy exist before delete the original one
                             const copiedStats = await fs.promises.stat(destFilePath);
                             if (copiedStats.size === stats.size) {
